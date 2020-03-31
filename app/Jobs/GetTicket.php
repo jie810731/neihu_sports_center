006d9574d45e070f420040e0c3b96fee966f73b6
JosephTang <jie810731@gmail.com>
2020-03-31 19:33:36 +0800

    1

--- app/Jobs/GetTicket.php
+++ app/Jobs/GetTicket.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
+use Log;
 
 class GetTicket implements ShouldQueue
 {
